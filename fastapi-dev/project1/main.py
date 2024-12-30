ccc11526b45feeb928b277848ed6e1e873b6ed62
idibyaranjan15 <dibyaranjansahoo696@gmail.com>
2024-12-30 16:28:46 +0530

    setup:done

--- fastapi-dev/project1/main.py
+++ fastapi-dev/project1/main.py
@@ -2,4 +2,21 @@ from fastapi import FastAPI
 app=FastAPI()
 @app.get("/")
 def index():
-    return 'heyy'
+    return {
+        "data":"blog list"
+    }
+
+@app.get("/blog/unpublished")
+def unpublished():
+    return {
+        "data":"all unpublished blogs"
+    }
+@app.get("/blog/{id}")
+def show(id:int):
+    return {"data":id}
+@app.get("/blog/{id}/comments")
+def comments(id):
+    return {"data":{
+        "1","2"
+    }}
+        
