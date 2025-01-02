1820ffb0c1ffa09ca70246e4b722c9bfa2e57a28
idibyaranjan15 <dibyaranjansahoo696@gmail.com>
2025-01-02 16:38:55 +0530

    fastapi:queryparameter

--- fastapi-dev/project1/main.py
+++ fastapi-dev/project1/main.py
@@ -1,22 +1,63 @@
 from fastapi import FastAPI
 app=FastAPI()
-@app.get("/")
-def index():
-    return {
-        "data":"blog list"
-    }
 
-@app.get("/blog/unpublished")
-def unpublished():
-    return {
-        "data":"all unpublished blogs"
+BOOKS = [
+    {
+        "title": "title One",
+        "author": "author One",
+        "category": "Science"
+    },
+    {
+        "title": "title Two",
+        "author": "author Two",
+        "category": "Arts"
+    },
+    {
+        "title": "title Three",
+        "author": "author Three",
+        "category": "Arts"
+    },
+    {
+        "title": "title Four",
+        "author": "author Four",
+        "category": "Commerce"
+    },
+    {
+        "title": "title Five",
+        "author": "author Five",
+        "category": "Arts"
+    },
+    {
+        "title": "title Six",
+        "author": "author Six",
+        "category": "Science"
+    },
+    {
+        "title": "title Seven",
+        "author": "author Seven",
+        "category": "Science"
+    },
+    {
+        "title": "title Eight",
+        "author": "author Eight",
+        "category": "Commerce"
+    },
+    {
+        "title": "title Nine",
+        "author": "author Nine",
+        "category": "Arts"
     }
-@app.get("/blog/{id}")
-def show(id:int):
-    return {"data":id}
-@app.get("/blog/{id}/comments")
-def comments(id):
-    return {"data":{
-        "1","2"
-    }}
-        
+]
+
+
+
+
+@app.get("/books/mybook")
+async def read_all_book():
+    return {"message":"This is an important message"}
+@app.get("/books/{book_title}")
+async def read_all_books(book_title:str):
+    for book in BOOKS:
+        if book.get("title").casefold()==book_title.casefold():
+            return book
+    
